3c6667e3fd97021381996969b623fcb9aa1959af
Gleb Dudarev <glebdydarev@mail.ru>
2022-02-11 11:35:35 -0600

    done

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ console.log(
 );
 
 // Exercise 1
+var age = 25;
+let firstname = "Gleb";
+const lastname = "Dudarev";
+
+
 
 // Exercise 2
 
+let fullName = firstname + " " + lastname;
+console.log(fullName)
+
 // Exercise 3
+
+let city = "Hampton"
+let passTime = "I have been at Olimpics!"
+
+ let myStory = `\Hello! My name is ${fullName}. I live in ${city}. I enjoy ${passTime} and coding!`;
+
+
+console.log(myStory)
